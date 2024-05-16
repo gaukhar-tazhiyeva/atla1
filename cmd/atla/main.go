06d99ecf99a3afc3768a6d7fcc204b1d0dfa45ab
gaukhar-tazhiyeva <123153107+gaukhar-tazhiyeva@users.noreply.github.com>
2024-05-16 12:58:28 +0500

    Update main.go

--- cmd/atla/main.go
+++ cmd/atla/main.go
@@ -33,7 +33,7 @@ func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 8081, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://postgres:postgres@localhost:5432/atla?sslmode=disable", "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://postgres:golang@localhost:5432/atla?sslmode=disable", "PostgreSQL DSN")
 	flag.Parse()
 
 	// Init logger
